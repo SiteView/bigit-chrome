a5975f66c5fef8fe85f90247ffa7c715b98b48fa
huyinghuan <ec.huyinghuan@gmail.com>
2014-02-21 18:09:05 +0800

    添加 bootstrap 框架 去google play 冗余按钮

--- extension/cpp/bigitAPI.cpp
+++ extension/cpp/bigitAPI.cpp
@@ -1,6 +1,6 @@
 /**********************************************************\
 
-  Auto-generated helloworldAPI.cpp
+  Auto-generated bigitAPI.cpp
 
 \**********************************************************/
 
@@ -8,19 +8,19 @@
 #include "variant_list.h"
 #include "DOM/Document.h"
 #include "global/config.h"
-#include "helloworldAPI.h"
+#include "bigitAPI.h"
 #include "fstream"
 #include <string>
 #include <iostream>
 #include <stdio.h>
 using namespace std;
 ///////////////////////////////////////////////////////////////////////////////
-/// @fn FB::variant helloworldAPI::echo(const FB::variant& msg)
+/// @fn FB::variant bigitAPI::echo(const FB::variant& msg)
 ///
 /// @brief  Echos whatever is passed from Javascript.
 ///         Go ahead and change it. See what happens!
 ///////////////////////////////////////////////////////////////////////////////
-FB::variant helloworldAPI::echo(const FB::variant& msg)
+FB::variant bigitAPI::echo(const FB::variant& msg)
 {
     static int n(0);
     fire_echo("So far, you clicked this many times: ", n++);
@@ -33,16 +33,16 @@ FB::variant helloworldAPI::echo(const FB::variant& msg)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-/// @fn helloworldPtr helloworldAPI::getPlugin()
+/// @fn bigitPtr bigitAPI::getPlugin()
 ///
 /// @brief  Gets a reference to the plugin that was passed in when the object
 ///         was created.  If the plugin has already been released then this
 ///         will throw a FB::script_error that will be translated into a
 ///         javascript exception in the page.
 ///////////////////////////////////////////////////////////////////////////////
-helloworldPtr helloworldAPI::getPlugin()
+bigitPtr bigitAPI::getPlugin()
 {
-    helloworldPtr plugin(m_plugin.lock());
+    bigitPtr plugin(m_plugin.lock());
     if (!plugin) {
         throw FB::script_error("The plugin is invalid");
     }
@@ -50,28 +50,28 @@ helloworldPtr helloworldAPI::getPlugin()
 }
 
 // Read/Write property testString
-std::string helloworldAPI::get_testString()
+std::string bigitAPI::get_testString()
 {
     return m_testString;
 }
 
-void helloworldAPI::set_testString(const std::string& val)
+void bigitAPI::set_testString(const std::string& val)
 {
     m_testString = val;
 }
 
 // Read-only property version
-std::string helloworldAPI::get_version()
+std::string bigitAPI::get_version()
 {
     return FBSTRING_PLUGIN_VERSION;
 }
 
-void helloworldAPI::testEvent()
+void bigitAPI::testEvent()
 {
     fire_test();
 }
 
-std::string helloworldAPI::exec(const char* cmd) {
+std::string bigitAPI::exec(const char* cmd) {
     FILE* pipe = popen(cmd, "r");
     if (!pipe) return "ERROR";
     char buffer[128];
@@ -83,11 +83,22 @@ std::string helloworldAPI::exec(const char* cmd) {
     pclose(pipe);
     return result;
 }
+std::string getExec(string appid){
+    string execStr("python ~/workspace/chrome/gplay-downloader.py --email ec.huyinghuan@gmail.com --password 0O.O0-00O --name @appid@ --country China --operator 'China Mobile' --device 32F64DC4664CF48F");
+    string dest("@appid@");
+    execStr.replace(execStr.find(dest),dest.size(),appid);
+    return execStr;
+}
 
-FB::variant helloworldAPI::mytest(const FB::variant& msg)
+FB::variant bigitAPI::mytest(const FB::variant& msg)
 {
     //FB::variant a = exec("~/workspace/c++/hello");
-    FB::variant a = exec("~/workspace/c++/hello");
+    string appid = msg.convert_cast<string>();
+   string execstr = getExec(appid);
+//   string downloadURL = exec( "python ~/workspace/chrome/gplay-downloader.py --email ec.huyinghuan@gmail.com --password 0O.O0-00O --name com.goldrun.snaps --country China --operator 'China Mobile' --device 32F64DC4664CF48F");
+    const char * execchar = execstr.c_str();
+    //std::string execstr = ~/workspace/c++/hello
+    FB::variant downloadURL = exec(execchar);
  //   printf("S:\t%s\n", a.c_FB::variantstr());
-    return a; 
+    return downloadURL; 
 }
