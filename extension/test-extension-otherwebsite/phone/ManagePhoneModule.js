a0ad48cc36d03f6be4c6c3bea25bea911e3188d0
ec <ec.huyinghuan@gmail.com>
2014-02-28 01:57:47 +0800

    添加 app 管理设计 添加router 等

--- extension/test-extension-otherwebsite/phone/ManagePhoneModule.js
+++ extension/test-extension-otherwebsite/phone/ManagePhoneModule.js
@@ -1,19 +1,26 @@
 //该Service与 插件通讯 。
 angular.module('PhoneManageApp.services', [])
     .factory('phoneManageService',function(){ //手机管理服务
+        var appCount = 10;
         //获取App列表
         var getAppList = function(){}
         //获取文件列表
         var getFileList = function(){}
         //获取App数量
         var getAppCount = function(){
-            return 10;
+            return appCount;
+        }
+        //
+        var setAppCount = function(num){
+            console.log('setAppCount' + num);
+            appCount = num;
         }
         //...  其他函数待定义
         return {
             'getAppList':getAppList,
             'getFileList':getFileList,
-            'getAppCount':getAppCount
+            'getAppCount':getAppCount,
+            'setAppCount':setAppCount
         }
     })
     .factory('phoneConnectService',function(){ //手机状态服务
@@ -27,8 +34,22 @@ angular.module('PhoneManageApp.services', [])
         }
     });
 
+//应用管理
+var AppsManagerModule = angular.module('AppsManagerModule',['PhoneManageApp.services']);
+AppsManagerModule.controller("AppsManagerModuleCtrl",
+    [
+        '$scope',
+        'phoneManageService',
+        function($scope,phoneManageService) {
+            $scope.delete = function(){
+                console.log(123);
+                phoneManageService.setAppCount(phoneManageService.getAppCount()-1);
+            } 
+        }
+    ]);
+
 //侧边栏的 小通知，如 应用数量等。dom click listener
-var SideNavModule = angular.module('SideNavModule',['PhoneManageApp.services']);
+var SideNavModule = angular.module('SideNavModule',['PhoneManageApp.services','ngRoute','AppsManagerModule']);
 SideNavModule.controller("SideNavModuleController",
 	[
 		'$scope',
@@ -36,59 +57,29 @@ SideNavModule.controller("SideNavModuleController",
         'navButtonActionService',
 		function($scope,phoneManageService,navButtonActionService) {
         	$scope.appCounts = phoneManageService.getAppCount();
-            $scope.navAction = navButtonActionService.navAction;
+            $scope.changeNavStyle = navButtonActionService.changeNavStyle;
     	}
     ]);
 
 SideNavModule.factory('navButtonActionService',function(){ //手机管理服务
-    var SideNavAction = {
-        "changeNavStyle":function(did){ // dom id
-            did = "#"+did;
-            $(did).parent().find(".active").removeClass('active');
-            $(did).addClass('active');
-        },
-        "doNavigtion":function(did){
-            SideNavAction.changeNavStyle(did);
-            var action = did.replace("nav_","goto");
-            SideNavAction[action]();
-        },
-        "gotoHome":function(){
-            console.log("gotoHome");
-        },
-        "gotoAppStore":function(){
-            console.log("gotoAppStore");
-        },
-        "gotoMyApp":function(){
-            console.log("gotoMyApp");
-        },
-        "gotoMyContacts":function(){
-            console.log("gotoMyContacts");
-        },
-        "gotoMyMusic":function(){
-            console.log("gotoMyMusic");
-        },
-        "gotoMyPictures":function(){
-            console.log("gotoMyPictures");
-        },
-        "gotoMyVideo":function(){
-            console.log("gotoMyVideo");
-        },
-        "gotoMySms":function(){
-            console.log("gotoMyMySms");
-        },
-        "gotoMyFiles":function(){
-            console.log("gotoMyFiles");
-        }
-    };
-
-    var navAction = function(domId){
-        SideNavAction.doNavigtion(domId);
+    var changeNavStyle = function(e){
+        $(e.currentTarget).parent().find(".active").removeClass('active');
+        $(e.currentTarget).addClass('active');
     }
-
-    return {
-        'navAction':navAction
-    }
+    return {'changeNavStyle':changeNavStyle};
 });
+
+SideNavModule.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider
+        .when('/MyApps/:appId', {
+            templateUrl: 'phone/MyApps.html'
+            ,controller: 'AppsManagerModuleCtrl'
+        })
+        .when('/MyFiles', {
+            templateUrl: 'phone/MyFiles.html'
+        })
+  }]);
 
 //连接手机的状态
 var PhoneConnectStatusModule = angular.module('PhoneConnectStatusModule',['PhoneManageApp.services']);
@@ -101,6 +92,7 @@ PhoneConnectStatusModule.controller("PhoneConnectStatusModuleCtrl",
         	$scope.status = phoneConnectService.getPhoneConnectStatus();
     	}
     ]);
+
 
 
 
