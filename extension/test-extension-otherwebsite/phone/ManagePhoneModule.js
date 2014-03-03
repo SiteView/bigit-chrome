f08afdd4acd77b1515d806faa2cecd490e9e3811
huyinghuan <ec.huyinghuan@gmail.com>
2014-03-04 01:50:30 +0800

    完成一些前台功能设计

--- extension/test-extension-otherwebsite/phone/ManagePhoneModule.js
+++ extension/test-extension-otherwebsite/phone/ManagePhoneModule.js
@@ -11,24 +11,24 @@ AppPluginServices.factory('appPluginService',function(){
     var PhoneProtoBuilder = ProtoBuf.loadProtoFile("phone/phone.proto");
    
     var testPlugin = function(){
-    // console.log( plugin.GetDeviceInfo("shell ls   /storage/sdcard0").toString());
-    var AppListMessage = PhoneProtoBuilder.build('bigit');
-   
-   // var message = plugin.DoTest('');
-  // var message = plugin.GetDeviceInfo('');
-   var message = plugin.GetAppList('');
+        // console.log( plugin.GetDeviceInfo("shell ls   /storage/sdcard0").toString());
+        var AppListMessage = PhoneProtoBuilder.build('bigit');
+       
+       // var message = plugin.DoTest('');
+      // var message = plugin.GetDeviceInfo('');
+        var message = plugin.GetAppList('');
 
-       console.log('message is:');
+        console.log('message is:');
         console.log(message);
-       console.log('builder is: ');
-       console.log(encodeURIComponent(message))
-       return;
-       message = btoa(encodeURIComponent(message));
-       var msg = AppListMessage.AppList.decode64(message);
-    //    var msg = AppListMessage.test.decode64(message);
-   //var msg = AppListMessage.DeviceInfo.decode64(message);
-       console.log('decode message  is:');
-         console.log(msg);
+        console.log('builder is: ');
+        // console.log(encodeURIComponent(message))
+        //   return;encodeURIComponent(message)
+        message = btoa(message);
+        var msg = AppListMessage.AppList.decode64(message);
+        //    var msg = AppListMessage.test.decode64(message);
+        //var msg = AppListMessage.DeviceInfo.decode64(message);
+        console.log('decode message  is:');
+        console.log(msg);
     }
     /* */
     return {
@@ -59,6 +59,16 @@ angular.module('PhoneManageApp.services', ['AppPlugin.services'])
             // appPlugin do some thing
             return appsCount;
         }
+
+        //获取一个app信息
+        var getAppDetail = function(appId){
+            //test
+            return {
+                appId:appId,
+                appName:appId+"_test"
+            }
+        }
+
         //卸载app
         var uninstall  = function(appId){
             //模拟卸载arrayObject.splice(index,howmany
@@ -69,6 +79,17 @@ angular.module('PhoneManageApp.services', ['AppPlugin.services'])
                     break;
                 }
             }
+        }
+        //测试添加
+        var addTest = function(){
+            appList.push({
+                "id":"4",
+                "name":"app_4", 
+                "version":"0.4", 
+                "size":"100", 
+                "location":"sdcard", 
+                "icodata":"6"
+            });
         }
 
         var _init = function(){
@@ -99,16 +120,18 @@ angular.module('PhoneManageApp.services', ['AppPlugin.services'])
                     "icodata":"6"
                 }
             ];
-            appsCount = appList.length;
+            //appsCount = appList.length;
         }
         _init();
         service = {
             'getAppList':getAppList,
+            'getAppDetail':getAppDetail,
             'getFileList':getFileList,
             'getAppCount':getAppCount,
             'uninstall':uninstall,
             'appsCount':appsCount,
-            'appList':appList
+            'appList':appList,
+            'addTest':addTest
         }
         //...  其他函数待定义
         return service;
@@ -129,21 +152,35 @@ var AppsManagerModule = angular.module('AppsManagerModule',['PhoneManageApp.serv
 AppsManagerModule.controller("AppsManagerModuleCtrl",
     [
         '$scope',
+        '$routeParams',
         'phoneManageService',
-        function($scope,phoneManageService) {
+        function($scope,$routeParams,phoneManageService) {
+            var refreshAppList = function(){
+                //模拟刷新数据
+               // phoneManageService.appsCount = phoneManageService.getAppCount();
+                phoneManageService.appsList = phoneManageService.getAppList();
+                phoneManageService.appsCount = phoneManageService.appsList.length;
+            }
             $scope.phoneManageService = phoneManageService;
             $scope.uninstall = function(appId){
                 phoneManageService.uninstall(appId);
-                //模拟刷新数据
-                phoneManageService.appsCount = phoneManageService.getAppCount();
-                phoneManageService.appsList = phoneManageService.getAppList();
+                refreshAppList();
+            };
+            //刷新
+            $scope.refreshAppList = function(){
+                // 测试
+                phoneManageService.addTest();
+                refreshAppList();
             } 
-
+            //获取某个App的信息
+            $scope.appDetails = phoneManageService.getAppDetail($routeParams.appid);
         }
     ]);
 
 //侧边栏的 小通知，如 应用数量等。dom click listener
-var SideNavModule = angular.module('SideNavModule',['PhoneManageApp.services','ngRoute','AppsManagerModule']);
+var SideNavModule = angular.module('SideNavModule',
+    ['PhoneManageApp.services','ngRoute','AppsManagerModule']);
+
 SideNavModule.controller("SideNavModuleController",
 	[
 		'$scope',
@@ -163,12 +200,17 @@ SideNavModule.factory('navButtonActionService',function(){ //手机管理服务
     return {'changeNavStyle':changeNavStyle};
 });
 
-SideNavModule.config(['$routeProvider',
-  function($routeProvider) {
+SideNavModule.config(['$compileProvider','$routeProvider',
+  function($compileProvider,$routeProvider) {
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(chrome-extension):/);
     $routeProvider
-        .when('/MyApps/:appId', {
-            templateUrl: 'phone/MyApps.html'
-            ,controller: 'AppsManagerModuleCtrl'
+        .when('/MyApps/AppList', {
+            templateUrl: 'phone/MyApps.html',
+            controller: 'AppsManagerModuleCtrl'
+        })
+        .when('/MyApps/AppDetails/:appid',{
+            controller:'AppsManagerModuleCtrl',
+            templateUrl:'phone/AppDetails.html'
         })
         .when('/MyFiles', {
             templateUrl: 'phone/MyFiles.html'
@@ -190,10 +232,12 @@ PhoneConnectStatusModule.controller("PhoneConnectStatusModuleCtrl",
 
 
 
-var PhoneManageApp = angular.module('PhoneManageApp',[
-    'PhoneConnectStatusModule',
-    'SideNavModule',
-    'ManagePhoneFilter']);
+var PhoneManageApp = angular.module('PhoneManageApp',
+    [
+        'PhoneConnectStatusModule',
+        'SideNavModule',
+        'ManagePhoneFilter'
+    ]);
 
 //自定义指令
 PhoneManageApp.directive('bigitSidenavbar', function() { //侧边导航栏
@@ -208,3 +252,12 @@ PhoneManageApp.directive('bigitSidenavbar', function() { //侧边导航栏
             templateUrl: 'phone/ManagePhoneTopNavBar.html'
         };
     });
+$(function(){
+    function __init(){
+        var scope = $('div[ng-controller=AppsManagerModuleCtrl]').scope();
+        scope.refreshAppList();
+        scope.$apply();
+    }
+    setTimeout(__init,2000);
+    
+});
