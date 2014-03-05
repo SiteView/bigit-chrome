63fb706ac2cf33a08c154f2a501761534f3e44dd
zhuqing <qing.zhu@dragonflow.com>
2014-03-05 16:29:49 +0800

    complete contextMenus of img and url

--- extension/demo-extension/contextMenuTest/background.js
+++ extension/demo-extension/contextMenuTest/background.js
@@ -1,38 +1,67 @@
+var menulist = [{"name":"Search \"%s\"","id":"searchWord","contexts":"selection","searchKinds":[
+		{"title":"Search baidu","id":"searchbaidu","contexts":"selection",},
+		{"title":"Search APK","id":"searchapk","contexts":"selection",}
+		]},
+		{"name":"Search image","id":"searchImg","contexts":"image","searchKinds":[
+		{"title":"download to phones","id":"downloadImg","contexts":"image",},
+		{"title":"show on DLNA","id":"showWlan","contexts":"image",},
+		{"title":"save to 私有云","id":"savetocloud","contexts":"image",}
+		]},
+		{"name":"Search url","id":"searchUrl","contexts":"link","searchKinds":[]},
+]
+
 function createContextMenu(){
 	console.log("create context menus");
 	
 	//创建菜单
-	var context = "selection";
-	var id = chrome.contextMenus.create({
-		"title":"Search",
-		"id":"searchId",
-		"contexts":[context]
-	});
 	
-	chrome.contextMenus.create({
-		"title":"Search baidu",
-		"id":"s1",
-		"parentId":id,
-		"contexts":[context],
-		"onclick":onClickMenu
-	});
-	chrome.contextMenus.create({
-		"title":"Search APK",
-		"id":"s2",
-		"parentId":id,
-		"contexts":[context],
-		"onclick":onClickMenu
-	});
+	for(var i = 0;i < menulist.length;i++){
+		var menus = menulist[i];
+		//console.log(menus);
+		var id = "";
+		if(menus.searchKinds.length != 0){
+			console.log("for kinds");
+			id = chrome.contextMenus.create({
+				"title":menus.name,
+				"id":menus.id,
+				"contexts":[menus.contexts]
+			});
+			for(var j = 0;j < menus.searchKinds.length;j++){
+				var menukind = menus.searchKinds[j];
+				var t = chrome.contextMenus.create({
+					"title":menukind.title,
+					"id":menukind.id,
+					"parentId":id,
+					"contexts":[menukind.contexts],
+					"onclick":onClickMenu
+				});
+			}
+		}else{
+			 chrome.contextMenus.create({
+				"title":menus.name,
+				"id":menus.id,
+				"contexts":[menus.contexts],
+				"onclick":onClickMenu
+			});
+		}
+		
+	}
 }
 
 function onClickMenu(info, tab){
 	var url = "";
 	var searchId = info.menuItemId;
 	console.log(searchId);
-	if(searchId == "s1"){
+	if(searchId == "searchbaidu"){
 		url = "http://www.baidu.com/s?wd="+info.selectionText;
-	}else if(searchId == "s2"){
-		url = "http://www.sogou.com/web?query="+info.selectionText;
+	}else if(searchId == "searchapk"){
+		url = "https://play.google.com/store/search?q="+info.selectionText;
+	}else if(searchId == "searchUrl"){
+		//console.log("search url");
+		//console.log(info.linkUrl);
+		url = info.linkUrl;
+	}else{
+		return;	
 	}
 	chrome.tabs.create({"url":url,"index":tab.index + 1});
 }
@@ -46,18 +75,14 @@ function init(){
 	deleteContextMenu();
 	createContextMenu();
 	console.log("test");
-	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-		console.log("request is :");
-		console.log(request);
+	// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+		// console.log("request is :");
+		// console.log(request);
 		
-		if (request.cmd == 'encode_text')
-		{
-			sendResponse(urlEncode(request.text));
-		}
-		if(request.order == "get_options"){
-			sendResponse({reponse:"good"});
-		}
-	});
+		// if(request.order == "get_options"){
+			// sendResponse({reponse:"good"});
+		// }
+	// });
 }
 
 init();
