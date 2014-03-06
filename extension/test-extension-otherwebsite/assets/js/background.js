28e248220f7056812cc98cde80137dffe37801df
huyinghuan <ec.huyinghuan@gmail.com>
2014-03-06 18:13:02 +0800

    监听下载完成事件

--- extension/test-extension-otherwebsite/assets/js/background.js
+++ extension/test-extension-otherwebsite/assets/js/background.js
@@ -9,18 +9,81 @@ function getDownloadAppUrl(appid){
     var storeurl = storelist[_defaultStore_];
     return storeurl.replace(/\@appName\@/,appid);
 }
-
-function downloadApp(appId){
-	var downloaURL = getDownloadAppUrl(appId);
-	var filename = appId + ".apk";
-	chrome.downloads.download({
-                url:downloaURL,
-                filename:filename
- 	}, function(downloadId){
-      	     console.log("download item's id is " + downloadId);
-	})
+//添加等待下载的任务
+function addApkToDownloadStorge(downloadId){
+    chrome.storage.local.get('apkWaitForDownloadStack',function(item){
+        apkStack = item.apkWaitForDownloadStack;
+        apkStack = apkStack ? apkStack : [];
+        apkStack.push(downloadId);
+        chrome.storage.local.set({"apkWaitForDownloadStack":apkStack});
+    });
+}
+///删除等待下载的任务 
+function removeApkFormDownloadStorge(downloadId){
+    chrome.storage.local.get('apkWaitForDownloadStack',function(item){
+        apkStack = item.apkWaitForDownloadStack;
+        if(!apkStack || !apkStack.length){
+            return;
+        }
+        var length = apkStack.length;
+        for(var i = 0 ; i < length ; i++){
+            if(apkStack[i] == downloadId){
+                 apkStack.splice(i,1);
+                 break;
+            }
+        }
+        chrome.storage.local.set({"apkWaitForDownloadStack":apkStack});
+    });
 }
 
+//安装已下载完成的任务
+function installApk(downloadId){
+    chrome.downloads.search({id:downloadId}, function(item){
+        console.log("下载完成:");
+        console.log(item);
+    });
+}
+
+function downloadApp(appId){
+    var downloaURL = getDownloadAppUrl(appId);
+    var filename = appId + ".apk";
+    chrome.downloads.download({
+        url:downloaURL,
+        filename:filename
+    }, function(downloadId){
+         console.log("download item's id is " + downloadId);
+         addApkToDownloadStorge(downloadId);
+    })
+}
+/*
+*下载完成： 
+data{
+    endTime:{
+        current:"200--xxx"
+    }
+    id:xxx
+    state:{
+        current:"complete"
+    }
+}
+下载终止：
+data{
+    canResume:{current:false}
+    id:
+    error
+}
+*/
+chrome.downloads.onChanged.addListener(function(data){
+        console.log("download:");
+        console.log(data);
+        //下载完成
+        if(data && data.endTime && data.state && data.state.current == "complete"){
+            removeApkFormDownloadStorge(data.id);
+            installApk(data.id);
+        }else if(data.canResume && data.canResume.current && data.error){ //下载失败
+            removeApkFormDownloadStorge(data.id);
+        }
+});
 //获取google play 页面下载按钮发送的消息
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
