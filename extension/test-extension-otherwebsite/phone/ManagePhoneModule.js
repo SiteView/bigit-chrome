f19116eb7b68f809347a9cb566a6180bf7485320
zhuqing <qing.zhu@dragonflow.com>
2014-03-20 15:30:11 +0800

    modity table about

--- extension/test-extension-otherwebsite/phone/ManagePhoneModule.js
+++ extension/test-extension-otherwebsite/phone/ManagePhoneModule.js
@@ -75,6 +75,12 @@ PhoneManage.directive('bigitTopnavbar',function(){ //展示顶部导航栏
             templateUrl: 'phone/ManagePhoneTopNavBar.html'
         };
     });
+PhoneManage.directive('bigitContent',function(){ //展示顶部导航栏
+        return {
+            restrict: 'E',
+            templateUrl: 'phone/ManagePhoneContent.html'
+        };
+    });
 //应用管理
 PhoneManage.controller("AppsManagerModuleCtrl",
     [
